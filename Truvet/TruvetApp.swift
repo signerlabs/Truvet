72e90e8a920717fd68dd86ad77d3cbd3cd6d3440
w-zhong <wei@signerlabs.com>
2025-10-29 18:33:44 +0800

    Initial Commit

---
+++ Truvet/TruvetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruvetApp.swift
+//  Truvet
+//
+//  Created by 仲炜 on 2025/10/29.
+//
+
+import SwiftUI
+
+@main
+struct TruvetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
